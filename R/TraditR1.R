26f4fb5a015dab115ab6d7e3c5a7e31db4a5014c
Ekaterina Smirnova <ekaterina.smirnova@mso.umt.edu>
2018-04-01 15:45:53 -0600

    Updated Files

--- R/TraditR1.R
+++ R/TraditR1.R
@@ -3,8 +3,8 @@
 ##############################################
 TraditR1 <- function(X, rel = FALSE, thresh=5){
   #Select without setting filtering criteria
-  TaxaAll <- names(Counts)
-  NNzero <- apply(Counts, 2, nnzero)
+  TaxaAll <- colnames(X)
+  NNzero <- apply(X, 2, nnzero)
   if(rel == TRUE){n <- dim(X)[1]
                   NNzero <- NNzero/n}
   filtX <- X[,NNzero >= thresh]
--- R/TraditR2.R
+++ R/TraditR2.R
@@ -2,22 +2,22 @@
 #Traditional Filtering Rule 2
 ##############################################
 TraditR2 <- function(X,  Ab_min = 0.001, prop =TRUE){
-  if(prop == FALSE) {X <- sweep(Counts, MARGIN=1, apply(Counts,1,sum), '/')}
+  if(prop == FALSE) {X <- sweep(X, MARGIN=1, apply(X,1,sum), '/')}
   n <- dim(X)[1]
   #Min abundance level is always 0, select max expression of a taxon
   Abund <- apply(X, 2, max)
   Nsamp <- apply(X, 2, nnzero)
   Psamp <- Nsamp/n
   #Select only taxa with abundance level > 0.001
-  if(is.null(names(Abund))){names(Abund) <- 1:length(Abund)}
-  Taxa <- names(Abund[Abund > Ab_min])  
+  if(is.null(colnames(Abund))){colnames(Abund) <- 1:length(Abund)}
+  Taxa <- colnames(Abund[Abund > Ab_min])  
   Abund <- Abund[Taxa]
   Nsamp <- Nsamp[Taxa]
   Psamp <- Psamp[Taxa]
   #Check additional conditions
-  C1 <- names(Abund[Abund > 0.01 & Nsamp >=1]) 
-  C2 <- names(Abund[Abund > 0.001 & Psamp >=0.02])
-  C3 <- names(Abund[Psamp >=0.05])
+  C1 <- colnames(Abund[Abund > 0.01 & Nsamp >=1]) 
+  C2 <- colnames(Abund[Abund > 0.001 & Psamp >=0.02])
+  C3 <- colnames(Abund[Psamp >=0.05])
   Taxa <- unique(c(C1, C2, C3))
   filtX <- X[,Taxa]
   return(filtX)
