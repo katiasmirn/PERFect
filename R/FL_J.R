1dc7f57149395bb6dab09455f3162bee4f3a3fc7
Ekaterina Smirnova <ekaterina.smirnova@mso.umt.edu>
2018-04-06 09:41:49 -0600

    Updated Files

--- R/FL_J.R
+++ R/FL_J.R
@@ -2,12 +2,12 @@
 #Calculate FL for taxa left in the set
 #################################
 FL_J <- function(X, J){
-  Ind <- which(names(X) %in%  J)
-  X_R <- X[,Ind]
+  Ind <- which(colnames(X) %in%  J)
+  X_R <- X[,-Ind]
   #calculate corresponding norm
   Netw <- t(as.matrix(X))%*%as.matrix(X)
   Netw_R <- t(as.matrix(X_R))%*%as.matrix(X_R)
-  FL <- 1 - tr(t(Netw_R)%*%Netw_R)/tr(t(Netw)%*%Netw)
+  FL <-  1 - (tr(t(Netw_R)%*%Netw_R)/tr(t(Netw)%*%Netw))
   
   return(FL)
 }
